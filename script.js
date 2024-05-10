35e98b8560747e60c7381a02408a4dcaf7dec553
Janryzamora <janryzamora25@gmail.com>
2024-05-10 22:37:06 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var subMenu = document.getElementById("profile-container");
+var leftMenu = document.getElementById("image-container");
+
+	function toggleMenu(){
+		subMenu.classList.toggle("open-menu");
+	}
+
+	function toggleMenu2(){
+		leftMenu.classList.toggle("open-menu");
+	}
+	
